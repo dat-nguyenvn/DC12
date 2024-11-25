f84f653be1676ee154cafd789d37a0aa0d407bd7
Dat Nguyen Ngoc <ah23975@bristol.ac.uk>
2024-11-25 20:14:34 +0000

    update25-11

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
 # DC12
+*A project that does something amazing.*
 # commit from laptop
 # commit fron PC
 # commit from jetson
+
+## Table of Contents
+1. [About the Project](#about-the-project)
+2. [Features](#features)
+3. [Getting Started](#getting-started)
+   - [Prerequisites](#prerequisites)
+   - [Installation](#installation)
+4. [Usage](#usage)
+### Setup environment
+```bash
+docker pull nvcr.io/nvidia/pytorch:23.12-py3
+
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="wildtracker",                      # Your project name
+    version="0.1.0",                        # Initial version
+    author="Ngoc Dat Nguyen",
+    author_email="dat.nguyenngoc@bristol.ac.uk",
+    description="A brief description of your project",
+    long_description=open("README.md").read(),  # Description from README
+    long_description_content_type="text/markdown",
+    url="https://github.com/dat-nguyenvn/DC12",  # Project's GitHub URL
+    packages=find_packages(),               # Automatically find packages
+    install_requires=[                      # Dependencies
+        "numpy",
+        "requests"
+    ],
+    classifiers=[                           # Optional metadata
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.9",                # Minimum Python version
+)
